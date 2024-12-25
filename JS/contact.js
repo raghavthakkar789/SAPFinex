ecf74654e5c7d3c6a656ae574c83544330bdaa36
raghavthakkar789 <bhavyathakkar96@gmail.com>
2024-12-26 02:54:55 +0530

    Final change remaining

--- JS/contact.js
+++ JS/contact.js
@@ -12,9 +12,27 @@ contactForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const nameValue = document.getElementById('name').value.trim();
   const emailValue = document.getElementById('email').value.trim();
+  const phoneValue = document.getElementById('phone').value.trim();
   const messageValue = document.getElementById('message').value.trim();
 
-  if (nameValue && emailValue && messageValue) {
+  if (nameValue && emailValue && phoneValue && messageValue) {
+    // Send the form data to the https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjYwNTZmMDYzMzA0MzE1MjZiNTUzNDUxMzUi_pc
+    // const formData = new FormData();
+    const formData = {
+      name: nameValue,
+      email: emailValue,
+      phone: phoneValue,
+      message: messageValue
+    };
+    console.log(formData);
+    const url = 'https://connect.pabbly.com/workflow/sendwebhookdata/IjU3NjYwNTZmMDYzMzA0MzE1MjZiNTUzNDUxMzUi_pc';
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
     alert('Thank you for contacting us! We will get back to you soon.');
     contactForm.reset();
   } else {
